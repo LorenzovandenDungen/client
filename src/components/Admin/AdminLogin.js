db3a0ca2a115ca94e51551fda38201a0b5e62e60
LorenzovandenDungen <2130766@talnet.nl>
2023-06-20 18:26:22 +0200

    save

--- src/components/Admin/AdminLogin.js
+++ src/components/Admin/AdminLogin.js
@@ -1,41 +1,28 @@
+// AdminLogin.js
 import React, { useState } from 'react';
 import axios from 'axios';
 
-function AdminLogin() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+const AdminLogin = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleSubmit = async event => {
-        event.preventDefault();
-
-        const user = {
-            email: email,
-            password: password
-        };
-
+    const handleSubmit = async (e) => {
+        e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/admin-login', user);
-            console.log(response.data);
-        } catch (error) {
-            console.error(`Error: ${error}`);
+            const res = await axios.post('/api/login', { email, password });
+            // Handle response (store user info, redirect, show error, etc.)
+        } catch (err) {
+            // Handle error
         }
     };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Email:
-                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
-                </label>
-                <label>
-                    Password:
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
-                </label>
-                <input type="submit" value="Login" />
-            </form>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            <button type="submit">Log In</button>
+        </form>
     );
-}
+};
 
 export default AdminLogin;
