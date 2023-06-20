52d31c612c5e48385bdaa550a287682e0737b782
LorenzovandenDungen <2130766@talnet.nl>
2023-06-20 17:50:02 +0200

    save

--- src/components/Admin/Invites.js
+++ src/components/Admin/Invites.js
@@ -1,1 +1,43 @@
- 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Invites = () => {
+  const [invites, setInvites] = useState([]);
+  const [userId, setUserId] = useState("");
+  const [trainingId, setTrainingId] = useState("");
+
+  useEffect(() => {
+    fetchInvites();
+  }, []);
+
+  const fetchInvites = async () => {
+    const response = await axios.get('http://localhost:5000/api/invites');
+    setInvites(response.data);
+  };
+
+  const addInvite = async () => {
+    await axios.post('http://localhost:5000/api/invites', {
+      userId,
+      trainingId,
+    });
+    fetchInvites();
+  };
+
+  return (
+    <div>
+      <h2>Invites</h2>
+      <form onSubmit={addInvite}>
+        <input type="text" value={userId} onChange={e => setUserId(e.target.value)} placeholder="User ID" />
+        <input type="text" value={trainingId} onChange={e => setTrainingId(e.target.value)} placeholder="Training ID" />
+        <button type="submit">Add Invite</button>
+      </form>
+      {invites.map(invite => (
+        <div key={invite.id}>
+          <p>User ID: {invite.userId} - Training ID: {invite.trainingId}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Invites;
--- src/components/Admin/Trainings.js
+++ src/components/Admin/Trainings.js
@@ -1,1 +1,44 @@
- 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Trainings = () => {
+  const [trainings, setTrainings] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    fetchTrainings();
+  }, []);
+
+  const fetchTrainings = async () => {
+    const response = await axios.get('http://localhost:5000/api/trainings');
+    setTrainings(response.data);
+  };
+
+  const addTraining = async () => {
+    await axios.post('http://localhost:5000/api/trainings', {
+      title,
+      content,
+    });
+    fetchTrainings();
+  };
+
+  return (
+    <div>
+      <h2>Trainings</h2>
+      <form onSubmit={addTraining}>
+        <input type="text" value={title} onChange={e => setTitle(e.target.value)} placeholder="Title" />
+        <textarea value={content} onChange={e => setContent(e.target.value)} placeholder="Content" />
+        <button type="submit">Add Training</button>
+      </form>
+      {trainings.map(training => (
+        <div key={training.id}>
+          <h3>{training.title}</h3>
+          <p>{training.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Trainings;
