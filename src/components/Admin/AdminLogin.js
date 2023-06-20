9ef21fbe48ee533d0d6a616d3554d89c9e539232
LorenzovandenDungen <2130766@talnet.nl>
2023-06-20 17:56:35 +0200

    save

--- src/components/Admin/AdminLogin.js
+++ src/components/Admin/AdminLogin.js
@@ -1,35 +1,41 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const AdminLogin = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+function AdminLogin() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async event => {
+        event.preventDefault();
 
-    // you should replace this URL with your own endpoint
-    const response = await axios.post("http://localhost:5000/api/admin/login", {
-      email,
-      password
-    });
+        const user = {
+            email: email,
+            password: password
+        };
 
-    if (response.data.authenticated) {
-      // The admin is authenticated
-      // You should save the admin's session info here
-    } else {
-      // The credentials were incorrect
-      // Show an error message here
-    }
-  };
+        try {
+            const response = await axios.post('http://localhost:5000/api/admin-login', user);
+            console.log(response.data);
+        } catch (error) {
+            console.error(`Error: ${error}`);
+        }
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button type="submit">Login</button>
-    </form>
-  );
-};
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Email:
+                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+                </label>
+                <label>
+                    Password:
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+                </label>
+                <input type="submit" value="Login" />
+            </form>
+        </div>
+    );
+}
 
 export default AdminLogin;
