1512d22371d92b4058c7d56d7182acd785e22b78
LorenzovandenDungen <2130766@talnet.nl>
2023-06-21 09:21:26 +0200

    save

--- src/components/Company/Config.js
+++ src/components/Company/Config.js
@@ -1,1 +1,21 @@
- 
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Config = () => {
+  const [config, setConfig] = useState({});
+
+  useEffect(() => {
+    axios.get("/api/config").then((res) => {
+      setConfig(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Config</h1>
+      <pre>{JSON.stringify(config, null, 2)}</pre>
+    </div>
+  );
+};  
+
+export default Config;
